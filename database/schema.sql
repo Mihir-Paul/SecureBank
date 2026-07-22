f8546f34bae7ca416ebf6dc0fa5cacd32e1bd9ee
Mihir-Paul <mihirpgm@gmail.com>
2026-07-22 19:47:39 +0530

    Designed Secure Dtaabase Schema

--- database/schema.sql
+++ database/schema.sql
@@ -14,3 +14,24 @@ CREATE TABLE customers(
     address TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE accounts(
+    account_number BIGINT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    account_type ENUM('savings','current') NOT NULL,
+    balance DECIMAL(12,2) DEFAULT 0.00,
+    pin_hash VARCHAR(255) NOT NULL,
+    status ENUM('Active','Blocked') DEFAULT 'Active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
+);
+
+CREATE TABLE transactions(
+    transaction_id INT PRIMARY KEY,
+    account_number BIGINT NOT NULL,
+    transaction_type ENUM('Deposit','Withdraw','Transfer') NOT NULL,
+    amount DECIMAL(12,2) NOT NULL,
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_number) REFERENCES accounts(account_number)
+);
---
+++ database/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO admins(
+    admin_id,
+    username,
+    password_hash,
+    full_name
+)
+VALUES(
+    1,
+    'admin',
+    'admin123',
+    'System Administrator'
+);
